7186e41f36ea43efa8adb607e86d3b48a3773465
Mehnoor Siddiqui <mehnoorsiddiqui9@gmail.com>
2023-01-02 17:20:36 +0000

    Update .env and index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get("/webhook", (req, res) => {
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    info: "WhatsApp API v1.0.2 | V⦿iceflow | 2022",
+    info: "WhatsApp API 2022",
     status: "healthy",
     error: null,
   });
